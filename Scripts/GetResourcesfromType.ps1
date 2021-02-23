eb31eb9f1083d332bde22500fba70511fca99d80
Alboroni <alexyo@microsoft.com>
2021-02-23 16:10:02 +0000

    tesr

---
+++ Scripts/GetResourcesfromType.ps1
@@ -0,0 +1,16 @@
+params (
+param ([Parameter(Mandatory)]$subscriptionId,[Parameter(Mandatory)] $resourcetype)
+
+)
+
+Select-AzSubscription -Subscription $subscriptionId
+
+$resourceIDs = (Get-AzResource -ResourceType $resourcetype).Id
+
+
+Write-Output "##vso[task.setvariable variable=$resIds;]$resourceIDs"
+		
+    ## Creates an output variable
+    
+Write-Output "##vso[task.setvariable variable=$resIds;isOutput=true]$resourceIDs"
+
