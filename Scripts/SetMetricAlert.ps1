6e1b8d35a93c3957fbaddc500fa4bd03e3207de6
Alboroni <alexyo@microsoft.com>
2021-03-03 13:12:26 +0000

    Update SetMetricAlert.ps1

--- Scripts/SetMetricAlert.ps1
+++ Scripts/SetMetricAlert.ps1
@@ -6,8 +6,8 @@ param (
 [Parameter(Mandatory)]  $actiongroups_id,
 [Parameter(Mandatory=$false)]$operator = 'GreaterThan',
 [Parameter(Mandatory=$false)]$aggregation = 'Average',
-[Parameter(Mandatory=$false)]$WindowSize = '00:10:00' ,
-[Parameter(Mandatory=$false)]$Frequency = '00:10:00' ,
+[Parameter(Mandatory=$false)]$WindowSize = '00:15:00' ,
+[Parameter(Mandatory=$false)]$Frequency = '00:15:00' ,
 [Parameter(Mandatory=$false)]$Threshold= '0' ,
 [Parameter(Mandatory=$false)]$Severity = '2' 
  
