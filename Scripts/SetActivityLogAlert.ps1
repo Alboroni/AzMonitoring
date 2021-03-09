cbe018bea0bf2872cb7b07e5283c02679925b554
Alboroni <alexyo@microsoft.com>
2021-03-09 12:30:48 +0000

    Update SetActivityLogAlert.ps1

--- Scripts/SetActivityLogAlert.ps1
+++ Scripts/SetActivityLogAlert.ps1
@@ -1,14 +1,53 @@
 param (
 [Parameter(Mandatory)]$subscriptionId,
 [Parameter(Mandatory)]  $resourcetype,
-[Parameter(Mandatory)] $metricname,
+#if targetting RGs (
+[Parameter(Mandatory=$false)] $resourcegroups,
 [Parameter(Mandatory)] $Alertname, 
 [Parameter(Mandatory)]  $actiongroups_id,
-[Parameter(Mandatory=$false)]$operator = 'GreaterThan',
-[Parameter(Mandatory=$false)]$aggregation = 'Average',
-[Parameter(Mandatory=$false)]$WindowSize = '00:15:00' ,
-[Parameter(Mandatory=$false)]$Frequency = '00:15:00' ,
-[Parameter(Mandatory=$false)]$Threshold= '0' ,
-[Parameter(Mandatory=$false)]$Severity = '2' 
- 
+[Parameter(Mandatory=$false)]$Category = 'Administrative',
+[Parameter(Mandatory)]$operationname 
+
+
  )
+
+
+ Function New-ActivityAlert ($altname)
+ {
+
+    Set-AzActivityLogAlert -Location 'Global' `
+    -Name $altname`
+    -ResourceGroupName 'azmonitoring'
+    -Scope $scope
+    -Action $actionGroupId
+    -Condition $condition1, $condition2
+
+ }
+
+
+ $condition1 = New-AzActivityLogAlertCondition -Field 'category' -Equal $Category
+$condition2 = New-AzActivityLogAlertCondition -Field 'operationName' -Equal $operationname
+
+$actionGroupId = New-AzActionGroup -ActionGroupId $actiongroups_id
+
+Select-AzSubscription -Subscription $subscriptionId
+
+
+#determine the scope for alerts
+if ($resourcegroups)
+{
+
+   #uses format  /subscriptions/00000000-0000-0000-0000-0000-00000000/resourceGroups/ResourceGroupName" 
+
+$scope =  '/subscriptions/' + $subscriptionId + '/resourceGroups/' + $resourcegroups
+
+
+
+}
+ 
+else
+{
+$scope = '/subscriptions/' + $subscriptionId
+
+
+}
