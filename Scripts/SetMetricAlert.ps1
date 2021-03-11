ed707443e0af95b8a4a17e903f12896c8f0725bd
Alboroni <alexyo@microsoft.com>
2021-03-11 09:52:08 +0000

    update threshold

--- Scripts/SetMetricAlert.ps1
+++ Scripts/SetMetricAlert.ps1
@@ -5,10 +5,10 @@ param (
 [Parameter(Mandatory)]$metricname,
 [Parameter(Mandatory)]$Alertname, 
 [Parameter(Mandatory)]$actiongroups_id,
-[Parameter(Mandatory=$false)]$operator = 'GreaterThan',
-[Parameter(Mandatory=$false)]$aggregation = 'Average',
+[Parameter(Mandatory=$false)]$Operator = 'GreaterThan',
+[Parameter(Mandatory=$false)]$Aggregation = 'Average',
 [Parameter(Mandatory=$false)]$WindowSize = '00:15:00' ,
-[Parameter(Mandatory=$false)]$Frequency = '00:15:00' ,
+[Parameter(Mandatory=$false)]$Frequency = '00:05:00' ,
 [Parameter(Mandatory=$false)]$Threshold= '0' ,
 [Parameter(Mandatory=$false)]$Severity = '2' 
  
