290c8341137d4ce8e8c80e5049cd7d3f5313d179
Alboroni <alexyo@microsoft.com>
2021-06-24 11:39:57 +0100

    Update SetActivityLogAlert.ps1

--- Scripts/SetActivityLogAlert.ps1
+++ Scripts/SetActivityLogAlert.ps1
@@ -26,8 +26,8 @@ write-host "Tpe os $type1"
 
 
 
-   #Set-AzActivityLogAlert -Location 'Global'  -Name $altname -ResourceGroupName  $resourcegroup -Scope $scope -Action $actionGroupId -Condition $condition1, $condition2, $condition3
-    Set-AzActivityLogAlert -Location 'Global'  -Name $altname -ResourceGroupName  $resourcegroup -Scope '/subscriptions/db99463c-2a00-433c-a39b-f63083b719a4/ResourceGroups/azmonitoringnew' , '/subscriptions/db99463c-2a00-433c-a39b-f63083b719a4/ResourceGroups/demodelete' -Action $actionGroupId -Condition $condition1, $condition2, $condition3
+   Set-AzActivityLogAlert -Location 'Global'  -Name $altname -ResourceGroupName  $resourcegroup -Scope $scope -Action $actionGroupId -Condition $condition1, $condition2, $condition3
+   # Set-AzActivityLogAlert -Location 'Global'  -Name $altname -ResourceGroupName  $resourcegroup -Scope '/subscriptions/db99463c-2a00-433c-a39b-f63083b719a4/ResourceGroups/azmonitoringnew' , '/subscriptions/db99463c-2a00-433c-a39b-f63083b719a4/ResourceGroups/demodelete' -Action $actionGroupId -Condition $condition1, $condition2, $condition3
 
 
 
