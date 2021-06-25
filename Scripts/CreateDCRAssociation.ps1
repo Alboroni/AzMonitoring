04ef9c783aeeec57332f73be23cae004fac8a275
Alboroni <alexyo@microsoft.com>
2021-06-25 17:28:23 +0100

    createassocia

---
+++ Scripts/CreateDCRAssociation.ps1
@@ -0,0 +1,41 @@
+param (
+[Parameter(Mandatory)]$subscriptionId,
+[Parameter(Mandatory)]$resourcegroup,
+[Parameter(Mandatory=$false)]$TargetRG, 
+[Parameter(Mandatory)] $DCRWindows_ID,
+[Parameter(Mandatory= $false)] $DCRLinux_ID,
+[Parameter(Mandatory=$false)] $linux
+
+
+
+ )
+
+ if ($targetRG)
+{
+$vmsdcr = get-azVM -ResourceGroupName $TargrtRG
+}
+
+else {
+
+$vmsdcr = Get-azVM
+
+}
+
+
+
+foreach ($vmdcr in $vmsdcr)
+{
+
+ If ($vmdcr.OsType -eq 'Windows')   
+{    
+New-AzDataCollectionRuleAssociation -TargetResourceId $vmdcr.Id -AssociationName "dcrwindowsAssoc" -RuleId $DCRWindows_ID
+}
+
+if ($vmdcr.OsType -eq 'Linux')  {
+
+    New-AzDataCollectionRuleAssociation -TargetResourceId $vmdcr.Id -AssociationName "dcrwlinuxAssoc" -RuleId $DCRLinux_ID
+
+}
+
+}
+
